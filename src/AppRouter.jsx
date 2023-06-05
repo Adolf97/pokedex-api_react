d2d2a5f375d8d803f349b8b3d3e74a763decebf0
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-06-05 10:56:08 -0600

    First commit

---
+++ src/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Navigation from './Components/Navigation'
+import HomePage from './Pages/HomePage'
+import PokemonPage from './Pages/PokemonPage'
+import SearchPage from './Pages/SearchPage'
+
+export default function AppRouter() {
+  return (
+    <Routes>
+
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<HomePage />} />
+        <Route path="pokemon/:id" element={<PokemonPage />} />
+        <Route path="search" element={<SearchPage />} />
+      </Route>
+      
+      <Route path="*" element={<Navigate to="/" />} />
+      
+    </Routes>
+  )
+}
